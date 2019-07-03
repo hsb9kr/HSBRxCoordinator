444dcf7c7c2773880da944bac026a77644f5a085
Sang bo Hong <hsb9kr@gmail.com>
2019-07-03 16:43:23 +0900

    open class

---
+++ Example/HSBRxCoordinator/AppCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  AppCoordinator.swift
+//  HSBRxCoordinator_Example
+//
+//  Created by Sang bo Hong on 03/07/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import HSBRxCoordinator
+
+class AppCoordinator: BaseCoordinator<Void>  {
+    
+}
